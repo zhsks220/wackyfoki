90ec7c27aa73926645422211fdf8996ea433eee8
zhsks220 <zhsks220@naver.com>
2025-06-17 04:29:25 +0900

    🗺️ Add sitemap.xml to root for SEO

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   i18n: {
-    defaultLocale: 'ko',
+    defaultLocale: 'en',
     locales: ['ko', 'en', 'ja', 'zh'],
     localeDetection: false
   },
